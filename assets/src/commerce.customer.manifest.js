5ca79cf10aa9524531ac789b05d205595963916e
ymle <ymle@kibocommerce.com>
2018-10-11 09:40:24 +0700

    Dynamically segment customers based on site usage patterns: Add debug logs

--- assets/src/commerce.customer.manifest.js
+++ assets/src/commerce.customer.manifest.js
@@ -8,5 +8,15 @@ module.exports = {
   'http.commerce.customer.accounts.addAccount.after': {
       actionName: 'http.commerce.customer.accounts.addAccount.after',
       customFunction: require('./domains/commerce.customer/http.commerce.customer.accounts.addAccount.after')
+  },
+  
+  'http.commerce.customer.accounts.addAccountAndLogin.before': {
+      actionName: 'http.commerce.customer.accounts.addAccountAndLogin.before',
+      customFunction: require('./domains/commerce.customer/http.commerce.customer.accounts.addAccountAndLogin.before')
+  },
+  
+  'http.commerce.customer.accounts.addAccountAndLogin.after': {
+      actionName: 'http.commerce.customer.accounts.addAccountAndLogin.after',
+      customFunction: require('./domains/commerce.customer/http.commerce.customer.accounts.addAccountAndLogin.after')
   }
 };
